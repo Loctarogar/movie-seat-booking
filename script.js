e93e5c2a3cde69fa91f63b1480a2df1395b7114f
Loctarogar <igrsl1990@gmail.com>
2021-08-20 23:35:32 +0200

    style: hover over seat fix

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ const seatNumber = document.getElementById("namber-of-seats");
 const ticketCost = document.getElementById("ticket-cost");
 
 document.addEventListener("click", (e) => {
-  if (e.target.classList == "seat") {
-    console.log("seat");
+  if (
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("occupied")
+  ) {
+    console.log("ff");
   }
 });
